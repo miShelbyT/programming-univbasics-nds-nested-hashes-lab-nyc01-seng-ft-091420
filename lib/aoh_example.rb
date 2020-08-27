6039aaf3712848e419372c9a310204dd8efa2597
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-27 20:58:46 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -33,9 +33,10 @@ end
 
 
 def aoh_lookup(aoh, row, key)
+  oah(row)[key]
 end
 
 def aoh_update(aoh, row, key, new_value)
+end
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
-end
